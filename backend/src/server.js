0548036abdd5bd47d93a5fbd1c5554759b6dbedd
Ujjwalrohilla12 <rohillaujjwal39@gmail.com>
2026-03-03 11:56:46 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,23 @@
 import express from "express";
-import{ENV} from "./lib/env.js"
+import path from "path";
+import { ENV } from "./lib/env.js";
 
 const app = express();
-
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
-  res.status(200).json({msg:"api is up and running"});
+  res.status(200).json({ msg: "API is up and running 🚀" });
 });
 
+// Production Static Serving
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+   });
+}
+
 app.listen(ENV.PORT, () => {
-  console.log("Server running on port:",ENV.PORT);
+  console.log("Server running on port:", ENV.PORT);
 });
